46d7981e0f40600b5d398278b952e3593a0aaac1
Valdeci Santos de Miranda <66446342+ValdeciMiranda@users.noreply.github.com>
2021-02-28 20:46:39 -0300

    Esse é o sétimo exercício de lógica
    Consiste em pegar três valores e ver se formam triângulos isósceles, equiláteros ou escaleno

---
+++ Exercicio_7/informarTipoTriangulo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace informarTipoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor, digite o primeiro valor do lado do triângulo: ");
+            double A = double.Parse(Console.ReadLine());
+
+            Console.Write("Por favor, digite o segundo valor do lado do triângulo: ");
+            double B = double.Parse(Console.ReadLine());
+
+            Console.Write("Por favor, digite o terceiro valor do lado do triângulo: ");
+            double C = double.Parse(Console.ReadLine());
+
+            if (A == B && A == C)
+            {
+                Console.WriteLine("Este triângulo é equilátero");
+            }
+
+            else if (A == B || A == C || B == C)
+            {
+                Console.WriteLine("Este triângulo é isósceles");
+            }
+
+            if (A != B && A != C && C != B)
+            {
+                Console.WriteLine("Este triângulo é escaleno");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
