074bec36d6e4040622ab25d6ceb2b8e3d3b000ae
Valdeci Santos de Miranda <66446342+ValdeciMiranda@users.noreply.github.com>
2021-03-01 01:04:28 -0300

    Adição do oitavo exercício
    O exercício é basicamente calcular a fatorial de 5 (que é 120) através do loop while ou enquanto.

---
+++ Exercicio_8/calculoFatorialCincoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoFatorialCincoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int numero = 5;
+            int fatorial = 1;
+
+            while(contador < numero)
+            {
+                contador++;
+                fatorial = fatorial * contador;
+            }
+
+            Console.WriteLine("A fatorial de 5 é " + fatorial);
+            Console.ReadKey();
+        }
+    }
+}
