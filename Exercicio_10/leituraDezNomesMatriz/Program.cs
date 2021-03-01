9edb9e8a8cdef5a81b19cf1ea2a593453c004239
Valdeci Santos de Miranda <66446342+ValdeciMiranda@users.noreply.github.com>
2021-03-01 02:40:09 -0300

    Decimo exercício adicionado
    O decimo exercício consiste numa matriz que armazena 10 nomes e depois os exibe na tela.

---
+++ Exercicio_10/leituraDezNomesMatriz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leituraDezNomesMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+            Console.Write("1 - Digite um nome: ");
+            nomes[0] = Console.ReadLine();
+            Console.Write("2 - Digite um nome: ");
+            nomes[1] = Console.ReadLine();
+            Console.Write("3 - Digite um nome: ");
+            nomes[2] = Console.ReadLine();
+            Console.Write("4 - Digite um nome: ");
+            nomes[3] = Console.ReadLine();
+            Console.Write("5 - Digite um nome: ");
+            nomes[4] = Console.ReadLine();
+            Console.Write("6 - Digite um nome: ");
+            nomes[5] = Console.ReadLine();
+            Console.Write("7 - Digite um nome: ");
+            nomes[6] = Console.ReadLine();
+            Console.Write("8 - Digite um nome: ");
+            nomes[7] = Console.ReadLine();
+            Console.Write("9 - Digite um nome: ");
+            nomes[8] = Console.ReadLine();
+            Console.Write("10 -Digite um nome: ");
+            nomes[9] = Console.ReadLine();
+
+            Console.WriteLine("Os nomes digitados foram");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
